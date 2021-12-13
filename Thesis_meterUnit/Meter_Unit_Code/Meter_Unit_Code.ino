fa6d4495712514b43299b49022958605f5432afc
Engr. Irvin Gil <theofficial.irvingil@gmail.com>
2021-12-13 17:01:37 +0800

    Successfully addded the working energy measuring function for the code on the meter unit.
    Also included the no user detection function
    to turn the system off when
    there is no one using the
    unit

--- Thesis_meterUnit/Meter_Unit_Code/Meter_Unit_Code.ino
+++ Thesis_meterUnit/Meter_Unit_Code/Meter_Unit_Code.ino
@@ -18,7 +18,6 @@ D - Update
 //code to support counting the array size
 #define ARRAY_SIZE(x) sizeof(x)/sizeof(x[0])
 /**------------------------------**/
-
 #define OLED_RESET 4   //dont know what this is for but it is important to be included
 #define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
 // define the values to be used in the keypad
@@ -32,7 +31,6 @@ char hexaKeys[ROWS][COLS] = {
 };
 byte rowPins[ROWS] = {2, 3, 4, 5};
 byte colPins[COLS] = {A0, A1, A2, A3};
-
 //code for initializing object using new library
 Adafruit_SH1106 display(OLED_RESET); 
 // object for PZEM 004T 
@@ -49,19 +47,25 @@ Keypad customKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS)
 
 #define relayPin 7  //used for the relay pin
 
-char user_input[4];
-int addressOnEEPROM= 0;
-short   setCursor_column = 0;
-short fixedNumberOfInputs = 0;
-char codeFromEEPROM[4];
+char user_input[4];   //array for getting the user input; this is the array where the values are stored
+int addressOnEEPROM= 0; //variable used in determining the address when looping through the eeprom memory
+short setCursor_column = 0;   //variable used as reference for the collumn when the user will be inputing the passcode
+short fixedNumberOfInputs = 0;  //variable used as reference to control the number of inputs from the user
+char codeFromEEPROM[4];   //array used where the retrieved values from eeprom are stored
 bool measureMode = false; //variable indicator to measure mode or not
 
-  //variables used for millis on measuring functions
-  const long thisInterval = 2000;
-  unsigned long previousMillis = 0;
+//variables used for millis on measuring functions
+const long thisInterval = 2000;
+unsigned long previousMillis = 0;
+  //millis for determining the minute
+  const long relayCutoff_Interval = 20000;
+  unsigned long determine_minuteInterval = 0;
+
+//variable to be used on the measuring energy function
+float timestamp_Energy = 0;
 
 
-
+ 
 //PRE DEFINED VALUES: {"1157","3727","6501","6698"}
 
 
@@ -92,6 +96,8 @@ void showInputPasscode(){
   display.println("Input Passcode:");
   display.display();
 }
+
+//method used to read stored codes in eeprom
 void readCodesFromEEEPROM(){
   String code = "";
   int indexCounter = 0;
@@ -113,6 +119,8 @@ void readCodesFromEEEPROM(){
 
   addressOnEEPROM = 1;
 }
+
+//method used to display text on the oled display to show the inputs from the user
 void showsUser_input(char key){
   // //set text color
   display.setTextColor(WHITE);
@@ -124,20 +132,20 @@ void showsUser_input(char key){
   setCursor_column += 12;
   // user_Input += key;
   display.display();
-
-
 }
-//This method is called when displaying messages: Access granted and Access Denied
+
+//This method is called when displaying messages: (Access granted and Access Denied)
 void showMessage(String message){
-display.clearDisplay();
+  display.clearDisplay();
   display.setTextColor(WHITE);
   display.setTextSize(2);
   display.setCursor(4,10);
   display.print(message);
   display.display();
-  delay(4000);
+  delay(2000);
   display.clearDisplay();
 }
+
 // this method gets the user input and compares it to the passcode values retrieved from the eeprom
 void checkInputAndDecide(){
   String compareThis, input;
@@ -153,6 +161,8 @@ void checkInputAndDecide(){
     }
     addressOnEEPROM+=5;
   }
+
+  //deciding action to determine the mode of the meter unit (Measure mode: ON / OFF)
   if(matchTrigger == false){
     showMessage("Error");
     addressOnEEPROM =1;
@@ -163,6 +173,13 @@ void checkInputAndDecide(){
     showMessage("Matched");
     addressOnEEPROM =1;
     measureMode =true;
+    delay(1000);
+    //get the accumulated measurement from the internal memory of the PZEM 004T
+    timestamp_Energy = pzem.energy();
+    //snapshot millis time for checking every minute
+    
+    
+    
   }
 
 }
@@ -178,91 +195,96 @@ String readStringFromEEPROM(int addrOffset)
   return String(data);
 }
 
-// method code for PZEM 004T to measure energy
-void measureEnergy(float current, float voltage, float power){
+//method to send the energy consumption data into the main unit
 
-  //instantiate current millis here
- 
-
-
-  display.clearDisplay();
-  float current = pzem.current();
-  float voltage = pzem.voltage();
-  float power = pzem.power();
-  float energy = pzem.energy();
-
-  unsigned long currentMillis = millis();
-
-  if (currentMillis - previousMillis >= thisInterval){
-    previousMillis = currentMillis;
-  
-    //for voltage display on serial
-    if(voltage != NAN){
-      Serial.print("Voltage: ");
-      Serial.print(voltage);
-      Serial.println("V");
-    } else {
-      Serial.println("Error reading voltage");
-    }
-    
-    //for Power display on serial
-    if(current != NAN){
-      Serial.print("Power: "); 
-      Serial.print(power); 
-      Serial.println("W");
-    } else {
-      Serial.println("Error reading power");
-    }
-
-    //for current display on serial
-    if(current != NAN){
-      Serial.print("Current: ");
-      Serial.print(current);
-      Serial.println("A");
-    } else {
-      Serial.println("Error reading current");
-    }
-    //for energy display on serial
-    if(current != NAN){
-      Serial.print("Energy: ");
-      Serial.print(energy,3); 
-      Serial.println("kWh");
-    } else {
-      Serial.println("Error reading energy");
-    }
-
-    Serial.println();
-    // delay(2000);
-
-
-    //displaying the values on OLED
-    display.clearDisplay();
-    display.setTextColor(WHITE);
-    display.setTextSize(1);
-    display.setCursor(2,1);
-    display.print("Energy: "+String(energy)+" kWh");
-
-    display.setTextSize(1);
-    display.setCursor(2,15);
-    display.print("Voltage: "+String(voltage)+" V");
-
-    display.setTextSize(1);
-    display.setCursor(2,28);
-    display.print("Power: "+String(power)+" W");
-
-    display.display();
-
+//method code for PZEM 004T to display the energy computed from the library
+void measureEnergy(float current, float voltage, float power, float energy){
+  //for voltage display on serial
+  if(voltage != NAN){
+    Serial.print("Voltage: ");
+    Serial.print(voltage,9);
+    Serial.println("V");
+  } else {
+    Serial.println("Error reading voltage");
   }
-  
 
+  //for Power display on serial
+  if(current != NAN){
+    Serial.print("Power: "); 
+    Serial.print(power,9); 
+    Serial.println("W");
+  } else {
+    Serial.println("Error reading power");
+  }
 
+  //for current display on serial
+  if(current != NAN){
+    Serial.print("Current: ");
+    Serial.print(current,9);
+    Serial.println("A");
+  } else {
+    Serial.println("Error reading current");
+  }
+
+  //for current energy display on serial
+  if(current != NAN){
+    Serial.print("Current Energy consumption: ");
+    Serial.print(energy,9); 
+    Serial.println("kWh");
+  } else {
+    Serial.println("Error current reading energy");
+  }
+  //for accumulated energy display on serial
+  if(current != NAN){
+    Serial.print("Accumulated Energy consumption: ");
+    Serial.print(energy + timestamp_Energy,9); 
+    Serial.println("kWh");
+  } else {
+    Serial.println("Error current reading energy");
+  }
+
+  Serial.println();
+  // delay(2000);
+
+  //displaying the values on OLED
+  display.clearDisplay();
+
+  display.setTextColor(WHITE);
+  display.setTextSize(1);
+  display.setCursor(2,1);
+  display.print("Energy: "+String(energy,5)+" kWh");
+
+  display.setTextSize(1);
+  display.setCursor(2,15);
+  display.print("Voltage: "+String(voltage,5)+" V");
+
+  display.setTextSize(1);
+  display.setCursor(2,28);
+  display.print("Power: "+String(power,5)+" W");
+
+  display.display();
+
+  //code to determine if the energy has changed for 1 minute
+  if (millis() < relayCutoff_Interval){
+      determine_minuteInterval = millis();
+  }
+  else if (millis() - determine_minuteInterval >= relayCutoff_Interval){
+    determine_minuteInterval = millis();  
+    //if the energy consumption remains unchanged for a minute, then the system trips the relay
+    if(power < 1){
+      //trip the relay off
+      Serial.print("Cutoff time has passed!");
+      digitalWrite(relayPin, LOW);
+      measureMode = false;
+      display.clearDisplay();
+    }       
+  }     
+    
 }
 
 
 
-
 //  = = = = = = = = = = = = set up and loop code begins here  = = = = = = = = = = = =
-  
 void setup() {
   Wire.begin();
   Serial.begin(9600);
@@ -276,78 +298,85 @@ void setup() {
   pinMode(relayPin, OUTPUT);  //setup from relay
 }
 
+
+
 void loop() {
 
   clearScreen:
   // put your main code here, to run repeatedly:
-  
   char keyValue = customKeypad.getKey();
 
   //setting mode for the system to execute
   if (measureMode == true ){ 
+    float current = pzem.current();
+    float voltage = pzem.voltage();
+    float power = pzem.power();
 
+    float energy = (pzem.energy() - timestamp_Energy);
    
-    if ((millis() - previousMillis) >= thisInterval){
-      previousMillis = millis();
-      measureEnergy();
+   //code for dealing with millis rollover that may cause possible bugs
+    if (millis() < thisInterval){
+      previousMillis = 0;
     }
+    else if (millis() - previousMillis >= thisInterval){
+      previousMillis = millis();
+      measureEnergy(current, voltage, power, energy);
+
+    }
+    
   }
   else{
     showInputPasscode();
     if(keyValue){
-    switch(keyValue){
-      case 'B':
-          display.clearDisplay();
-          setCursor_column = 0;
-          keyValue = 0x00;
-          memset(user_input, 0, sizeof(user_input));
-          fixedNumberOfInputs =0;
-          goto clearScreen;
-      break;
-
-      case 'A':
-          checkInputAndDecide();
-          setCursor_column = 0;
-          keyValue = 0x00;
-          memset(user_input, 0, sizeof(user_input));
-          fixedNumberOfInputs =0;
-          goto clearScreen;
-      break;
-      
-      //pressing *D means to end the measuring process of the unit
-      case 'D':
-          //Reset all values
-          display.clearDisplay();
-          setCursor_column = 0;
-          keyValue = 0x00;
-          memset(user_input, 0, sizeof(user_input));
-          fixedNumberOfInputs =0;
-          measureMode = false;
-          previousMillis = 0;
-
-
-          //Enter code for sending final data here
-
-
-
-
-          goto clearScreen;
-
-      break;
-
-      default:
-        if(fixedNumberOfInputs < 4){
-            user_input[fixedNumberOfInputs] = keyValue;
-            showsUser_input(keyValue);
+      switch(keyValue){
+        case 'B':
+            display.clearDisplay();
+            setCursor_column = 0;
             keyValue = 0x00;
-            fixedNumberOfInputs++;
-        }
-      break;
+            memset(user_input, 0, sizeof(user_input));
+            fixedNumberOfInputs =0;
+            goto clearScreen;
+        break;
+
+        case 'A':
+            checkInputAndDecide();
+            setCursor_column = 0;
+            keyValue = 0x00;
+            memset(user_input, 0, sizeof(user_input));
+            fixedNumberOfInputs =0;
+            goto clearScreen;
+        break;
+        
+        //pressing *D means to end the measuring process of the unit
+        case 'D':
+            // //Reset all values
+            // display.clearDisplay();
+            // setCursor_column = 0;
+            // keyValue = 0x00;
+            // memset(user_input, 0, sizeof(user_input));
+            // fixedNumberOfInputs =0;
+            // measureMode = false;
+            // previousMillis = 0;
+
+
+            // //Enter code for sending final data here
+            // goto clearScreen;
+
+        break;
+
+        default:
+          // will not accept user input from user once the fixedNumberOfInputs reaches 4
+          if(fixedNumberOfInputs < 4){
+              user_input[fixedNumberOfInputs] = keyValue;
+              showsUser_input(keyValue);
+              keyValue = 0x00;
+              fixedNumberOfInputs++;
+          }
+        break;
+      }
     }
+    // digitalWrite(relayPin, LOW);
   }
-  digitalWrite(relayPin, LOW);
-  }
-  
 
 
 }
