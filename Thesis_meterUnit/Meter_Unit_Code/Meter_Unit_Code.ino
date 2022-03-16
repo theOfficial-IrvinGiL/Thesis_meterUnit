4abd77a0ac075ca96eeef63c2311b2847198cc0f
Engr. Irvin Gil <theofficial.irvingil@gmail.com>
2022-03-16 21:32:40 +0800

    commit as of march 16, 2022: - added code to allow sending of data to main unit -

--- Thesis_meterUnit/Meter_Unit_Code/Meter_Unit_Code.ino
+++ Thesis_meterUnit/Meter_Unit_Code/Meter_Unit_Code.ino
@@ -1,9 +1,10 @@
+
+
 /**Notice:
 (function keys for the meter unit)
 A - Enter key
 B - Clear key
-C - End operations
-D - Update
+
 **/
 // #include <OnewireKeypad.h>
 //include libraries for the 128x32 oled display
@@ -14,6 +15,10 @@ D - Update
 #include <EEPROM.h>
 #include <Keypad.h>
 #include <PZEM004Tv30.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+#include <RTClib.h>//needs library
+
 /**supporting codes------------**/
 //code to support counting the array size
 #define ARRAY_SIZE(x) sizeof(x)/sizeof(x[0])
@@ -31,18 +36,25 @@ char hexaKeys[ROWS][COLS] = {
 };
 byte rowPins[ROWS] = {2, 3, 4, 5};
 byte colPins[COLS] = {A0, A1, A2, A3};
+
 //code for initializing object using new library
 Adafruit_SH1106 display(OLED_RESET); 
 // object for PZEM 004T 
 PZEM004Tv30 pzem(11, 12); //TX, RX
+//object for RF module
+RF24 radio(9, 10); // CE, CSN
+//object for keypad
+Keypad customKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS);
+//Setting the two addresses. One for transmitting and one for receiving
+const byte addresses [][6] = {"00001", "00002"};  
+//object for real-time clock
+RTC_DS3231 rtc;
 
 #if (SH1106_LCDHEIGHT != 64)
 #error("Height incorrect, please fix Adafruit_SH1106.h!");
 #endif
 
 
-//object for keypad
-Keypad customKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS);
 //Variable declarations ------------------------------------
 
 #define relayPin 7  //used for the relay pin
@@ -63,18 +75,13 @@ unsigned long previousMillis = 0;
 
 //variable to be used on the measuring energy function
 float timestamp_Energy = 0;
-
+//boolean variable for determining if the unit should be sending data
+boolean sendRF_mode = false;
+String RF_message = ""; //vessel variable for the message to send through RF
+String this_userContact = "";
 
  
 //PRE DEFINED VALUES: {"1157","3727","6501","6698"}
-
-
-//methods
-
-
-
-
-
 
 
 //method to send the energy consumption data into the main unit
@@ -85,6 +92,7 @@ float timestamp_Energy = 0;
 void setup() {
   Wire.begin();
   Serial.begin(9600);
+  nrfSetup();
   
   // by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
   display.begin(SH1106_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3D (for the 128x64)
--- Thesis_meterUnit/Meter_Unit_Code/process_codes.ino
+++ Thesis_meterUnit/Meter_Unit_Code/process_codes.ino
@@ -1,128 +1,209 @@
-//methods that are responsible for background process functions are written here:
+// methods that are responsible for background process functions are written here:
+
+// declaration for nrf module.(run once)
+void nrfSetup()
+{
+  radio.begin(); // Starting the radio communication
+  Serial.begin(9600);
+  radio.openWritingPipe(addresses[1]);    // Setting the address at which we will send the data
+  radio.openReadingPipe(1, addresses[0]); // Setting the address at which we will receive the data
+  radio.setPALevel(RF24_PA_MAX);          // You can set it as minimum or maximum depending on the distance between the transmitter and receiver.
+}
 
 // this method gets the user input and compares it to the passcode values retrieved from the eeprom
-void checkInputAndDecide(){
+void checkInputAndDecide()
+{
   String compareThis, input;
-  for(int x=0; x < ARRAY_SIZE(user_input); x++){
+  for (int x = 0; x < ARRAY_SIZE(user_input); x++)
+  {
     input += user_input[x];
   }
   boolean matchTrigger = false;
-  while(EEPROM.read(addressOnEEPROM)> 0){
+  while (EEPROM.read(addressOnEEPROM) > 0)
+  {
     compareThis = readStringFromEEPROM(addressOnEEPROM);
-    if(compareThis == input){
+    if (compareThis == input)
+    {
       matchTrigger = true;
       break;
     }
-    addressOnEEPROM+=5;
+    addressOnEEPROM += 5;
   }
-
-  //deciding action to determine the mode of the meter unit (Measure mode: ON / OFF)
-  if(matchTrigger == false){
+  // deciding action to determine the mode of the meter unit (Measure mode: ON / OFF)
+  if (matchTrigger == false)
+  {
     showMessage("Error");
-    addressOnEEPROM =1;
+    addressOnEEPROM = 1;
     measureMode = false;
-    
-  }else{
+  }
+  else
+  {
     digitalWrite(relayPin, HIGH);
     showMessage("Matched");
-    addressOnEEPROM =1;
-    measureMode =true;
+    addressOnEEPROM = 1;
+    measureMode = true;
     delay(1000);
-    //get the accumulated measurement from the internal memory of the PZEM 004T
+    // get the accumulated measurement from the internal memory of the PZEM 004T
     timestamp_Energy = pzem.energy();
-    //snapshot millis time for checking every minute
-    
-    
-    
+    // snapshot millis time for checking every minute
   }
-
 }
 
-//method code for PZEM 004T to display the energy computed from the library
-void measureEnergy(float current, float voltage, float power, float energy){
-  //for voltage display on serial
-  if(voltage != NAN){
+// method code for PZEM 004T to display the energy computed from the library
+void measureEnergy(float current, float voltage, float power, float energy)
+{
+  // for voltage display on serial
+  if (voltage != NAN)
+  {
     Serial.print("Voltage: ");
-    Serial.print(voltage,9);
+    Serial.print(voltage, 9);
     Serial.println("V");
-  } else {
+  }
+  else
+  {
     Serial.println("Error reading voltage");
   }
 
-  //for Power display on serial
-  if(current != NAN){
-    Serial.print("Power: "); 
-    Serial.print(power,9); 
+  // for Power display on serial
+  if (current != NAN)
+  {
+    Serial.print("Power: ");
+    Serial.print(power, 9);
     Serial.println("W");
-  } else {
+  }
+  else
+  {
     Serial.println("Error reading power");
   }
 
-  //for current display on serial
-  if(current != NAN){
+  // for current display on serial
+  if (current != NAN)
+  {
     Serial.print("Current: ");
-    Serial.print(current,9);
+    Serial.print(current, 9);
     Serial.println("A");
-  } else {
+  }
+  else
+  {
     Serial.println("Error reading current");
   }
 
-  //for current energy display on serial
-  if(current != NAN){
+  // for current energy display on serial
+  if (current != NAN)
+  {
     Serial.print("Current Energy consumption: ");
-    Serial.print(energy,9); 
+    Serial.print(energy, 9);
     Serial.println("kWh");
-  } else {
+  }
+  else
+  {
     Serial.println("Error current reading energy");
   }
-  //for accumulated energy display on serial
-  if(current != NAN){
+  // for accumulated energy display on serial
+  if (current != NAN)
+  {
     Serial.print("Accumulated Energy consumption: ");
-    Serial.print(energy + timestamp_Energy,9); 
+    Serial.print(energy + timestamp_Energy, 9);
     Serial.println("kWh");
-  } else {
+  }
+  else
+  {
     Serial.println("Error current reading energy");
   }
 
   Serial.println();
   // delay(2000);
 
-  //displaying the values on OLED
+  // displaying the values on OLED
   display.clearDisplay();
 
   display.setTextColor(WHITE);
   display.setTextSize(1);
-  display.setCursor(2,1);
-  display.print("Energy: "+String(energy,7)+" kWh");
+  display.setCursor(2, 1);
+  display.print("Energy: " + String(energy, 7) + " kWh");
 
   display.setTextSize(1);
-  display.setCursor(2,15);
-  display.print("Voltage: "+String(voltage,5)+" V");
+  display.setCursor(2, 15);
+  display.print("Voltage: " + String(voltage, 5) + " V");
 
   display.setTextSize(1);
-  display.setCursor(2,28);
-  display.print("Power: "+String(power,5)+" W");
+  display.setCursor(2, 28);
+  display.print("Power: " + String(power, 5) + " W");
 
   display.setTextSize(1);
-  display.setCursor(2,41);
-  display.print("Current: "+String(current,5)+" A");
+  display.setCursor(2, 41);
+  display.print("Current: " + String(current, 5) + " A");
 
   display.display();
 
-  //code to determine if the energy has changed for 1 minute
-  if (millis() < relayCutoff_Interval){
-      determine_minuteInterval = millis();
+  // code to determine if the energy has changed for 1 minute
+  if (millis() < relayCutoff_Interval)
+  {
+    determine_minuteInterval = millis();
   }
-  else if (millis() - determine_minuteInterval >= relayCutoff_Interval){
-    determine_minuteInterval = millis();  
-    //if the energy consumption remains unchanged for a minute, then the system trips the relay
-    if(power < 1){
-      //trip the relay off
+  else if (millis() - determine_minuteInterval >= relayCutoff_Interval)
+  {
+    determine_minuteInterval = millis();
+    // if the energy consumption remains unchanged for a minute, then the system trips the relay
+    if (power < 1)
+    {
+      // write code for storing the data values into a string variable ready for sending
+      String currentEnergy = String(energy, 7);
+      concatDateTime(currentEnergy);
+      sendTo_main(RF_message);
+
+      // trip the relay off
       Serial.print("Cutoff time has passed!");
       digitalWrite(relayPin, LOW);
       measureMode = false;
+      sendRF_mode = true;
       display.clearDisplay();
-    }       
-  }     
-    
+      RF_message = ""; // clear the global message variable
+    }
+  }
+}
+
+// code for concatinating the time and date into the RFmessage variable
+void concatDateTime(String energy_consumed)
+{
+  DateTime Thisnow = rtc.now();
+
+  RF_message += String(Thisnow.month(), DEC); // adding the date
+  RF_message += "/";
+  RF_message += String(Thisnow.day(), DEC);
+  RF_message += "/";
+  RF_message += String(Thisnow.year(), DEC);
+
+  RF_message += "_"; // buffer character
+
+  RF_message += String(Thisnow.hour(), DEC); // adding the time
+  RF_message += ":";
+  RF_message += String(Thisnow.minute(), DEC);
+
+  RF_message += "_";             // buffer character
+  RF_message += energy_consumed; // concat the current energy consumed data
+
+  // RF_message += "_"; // buffer character
+  // RF_message += this_userContact;
+}
+
+// method for sending data over RF to main unit
+void sendTo_main(String message)
+{
+  radio.stopListening(); // This sets the module as transmitter
+  int messageLength = message.length();
+  char data[messageLength];
+
+  /**
+   * gets data from global message variable and copies it to data 
+   * array for sending through RF
+   */
+  for (int x = 0; x < messageLength; x++)
+  {
+    data[x] = message[x];
+  }
+
+  // const char data[] = "First Data";
+  radio.write(&data, sizeof(data)); // Sending the data
+  delay(10);
 }
